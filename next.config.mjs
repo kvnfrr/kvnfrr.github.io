267ee6b81c697214d46bdf3f9ac1a6cd48485772
kvnfrr <kevinferrercs@gmail.com>
2024-12-21 09:45:29 -0500

    Adjusted for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+      output: 'export',
+      basePath: '',
+      trailingSlash: true, // Ensures all URLs end with a trailing slash
+    };
+    
+    module.exports = nextConfig;
+    
