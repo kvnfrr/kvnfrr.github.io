d28895fe2a3a0caeeb052707c279a79fac756c8a
kvnfrr <kevinferrercs@gmail.com>
2024-12-22 14:35:28 -0500

    New Framework and styling

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-  output: 'export',
-  basePath: '/kvnfrr.github.io',
-  assetPrefix: '/kvnfrr.github.io/',
-};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
+module.exports = {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+	  './public/**/*.html',
+	],
+	theme: {
+	  extend: {
+	    colors: {
+		'terminal-bg': '#121212',
+		'terminal-gray': '#1E1E1E',
+		'terminal-text': '#E5E5E5',
+		'accent-green': '#00FF7F',
+	    },
+	  },
+	},
+	plugins: [],
+    };
+    
