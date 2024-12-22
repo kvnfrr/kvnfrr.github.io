23a85c2afd58de822638447919ea324ff5c54617
kvnfrr <kevinferrercs@gmail.com>
2024-12-22 16:53:54 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  // Set the base path to '/' for a user site
+  base: '/',
+  
+  // Output the built site to the 'docs' directory
+  outDir: 'docs',
+  
+  // Integrate Tailwind CSS
   integrations: [tailwind()]
 });
