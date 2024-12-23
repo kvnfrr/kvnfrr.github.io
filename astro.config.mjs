5e18dc9fc4b3fa4031d69f05061f31777a89624d
kvnfrr <kevinferrercs@gmail.com>
2024-12-23 01:38:37 -0500

    added deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kvnfrr.github.io', // Your GitHub Pages site
-  base: '/kvnfrr.github.io/', // Repository name
   outDir: './docs', // Output to 'docs' folder
   
   // Integrate Tailwind CSS
