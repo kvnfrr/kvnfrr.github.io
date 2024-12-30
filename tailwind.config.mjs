2b7e3170f724504b85a9ed4220020b6d7be014de
kvnfrr <kevinferrercs@gmail.com>
2024-12-29 22:43:01 -0500

    Redesign

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,15 +7,8 @@ module.exports = {
 	theme: {
 	  extend: {
 	    colors: {
-		'terminal-bg': '#121212',
-		'terminal-gray': '#1E1E1E',
-		'terminal-text': '#E5E5E5',
-		'accent-green': '#00FF7F',
-		// Additional greys for the sections:
-		'section-grey-1': '#1A1A1A',
-		'section-grey-2': '#242424',
-		'section-grey-3': '#2E2E2E',
-		'section-grey-4': '#383838',
+		'background': '#1b1b1b',
+
 	    },
 	  },
 	},
