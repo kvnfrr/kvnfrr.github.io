e289192fa0e5cacb200fb46b30f0091d9debec53
kvnfrr <kevinferrercs@gmail.com>
2025-07-29 14:01:36 -0400

    Added Resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://kvnfrr.github.io',
   integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
+
