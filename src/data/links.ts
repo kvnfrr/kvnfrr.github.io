4d1fcc6f4a09e02df16f890b1dd16d00ad7dca4a
kvnfrr <kevinferrercs@gmail.com>
2025-11-25 00:40:32 -0500

    improved some text

---
+++ src/data/links.ts
@@ -0,0 +1,7 @@
+export const links = {
+  email: "mailto:kevinferrercs@gmail.com",
+  linkedin: "https://linkedin.com/in/kevinferrercs",
+  github: "https://github.com/kvnfrr",
+  handshake: "https://fiu.joinhandshake.com/profiles/kvnfrr",
+  resume: "/Kevin_s_Resume.pdf"
+};
